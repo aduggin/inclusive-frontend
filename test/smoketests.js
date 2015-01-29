1ce414a503684ead1184419b3f821be09e4ca687
Alistair Duggin <alistair.duggin@btinternet.com>
2015-01-29 11:51:31 +0000

    Rename list with styled numbers

--- test/smoketests.js
+++ test/smoketests.js
@@ -1,13 +1,13 @@
 module.exports = {
   'link to \'Styled list numbers\' is on the homepage': function (test) {
     test.open('http://0.0.0.0:9000/')
-      .assert.attr('#t-styled-list-numbers', 'href')
-        .to.contain('/styled-list-numbers.html', 'Link to \'Styled list numbers\' page found')
+      .assert.attr('#t-list-styled-numbers', 'href')
+        .to.contain('/list-styled-numbers.html', 'Link to \'Styled list numbers\' page found')
       .done();
   },
   '\'Styled list numbers\' page displays component': function (test) {
-    test.open('http://0.0.0.0:9000/styled-list-numbers.html')
-      .assert.exists('.styled-list-numbers')
+    test.open('http://0.0.0.0:9000/list-styled-numbers.html')
+      .assert.exists('.list-styled-numbers')
       .done();
   }
 };
